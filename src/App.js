64ebc796a44059a903f8b633610e8253ff10af3a
TRAORE ADAMA <traoreadamakoleba@gmail.com>
2024-02-29 14:49:15 +0100

    portfolio

--- src/App.js
+++ src/App.js
@@ -23,15 +23,15 @@ import "./assets/vendor/swiper/swiper-bundle.min.css";
 import "./MyFonction.jsx";
 
 // Vendor JS Files
-//import "./assets/vendor/purecounter/purecounter_vanilla.js";
-//import "./assets/vendor/aos/aos.js";
-//import "./assets/vendor/bootstrap/js/bootstrap.bundle.min.js";
-//import "./assets/vendor/glightbox/js/glightbox.min.js";
-//import "./assets/vendor/isotope-layout/isotope.pkgd.min.js";
-//import "./assets/vendor/swiper/swiper-bundle.min.js";
-//import "./assets/vendor/typed.js/typed.umd.js";
-//import "./assets/vendor/waypoints/noframework.waypoints.js";
-//import "./assets/vendor/php-email-form/validate.js";
+import "./assets/vendor/purecounter/purecounter_vanilla.js";
+import "./assets/vendor/aos/aos.js";
+import "./assets/vendor/bootstrap/js/bootstrap.bundle.min.js";
+import "./assets/vendor/glightbox/js/glightbox.min.js";
+import "./assets/vendor/isotope-layout/isotope.pkgd.min.js";
+import "./assets/vendor/swiper/swiper-bundle.min.js";
+import "./assets/vendor/typed.js/typed.umd.js";
+import "./assets/vendor/waypoints/noframework.waypoints.js";
+import "./assets/vendor/php-email-form/validate.js";
 
 function App() {
   return (
